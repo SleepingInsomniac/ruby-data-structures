35c7c67946dcc1a666327ab0bcae3d22dd2e75c9
Alex Clink <code@alexclink.com>
2018-06-25 23:27:43 -0400

    Fix union, add path compression

--- lib/union.rb
+++ lib/union.rb
@@ -2,45 +2,59 @@
 
 class Union
   class Node
-    attr_accessor :id, :data
+    attr_accessor :data, :parent, :children
 
-    def initialize(id, data, root = nil)
-      @id = id
+    def initialize(data, children = [], parent = nil)
       @data = data
-      @root = root
+      @children = children
+      @parent = parent
     end
 
-    def root=(value)
-      @root = @root ? @root.root : value
+    def root=(other)
+      if @parent
+        @parent.root = other.root
+      else
+        @parent = other.root
+      end
     end
 
     def root
-      @root ? @root.root : self
+      @parent ? @parent = @parent.root : self
+    end
+
+    def size
+      @children.length + @children.reduce(0) { |t, c| t + c.size }
+    end
+
+    def <<(other)
+      other.root = root
+      @children << other
     end
   end
 
   def initialize(*data)
-    @auto_increment = 0
     @nodes = []
     add(*data)
   end
 
-  def next_id
-    @auto_increment += 1
-  end
-
   def add(*data)
     data.each do |datum|
-      node = Node.new(next_id, datum)
+      node = Node.new(datum)
       @nodes << node
     end
   end
 
   def link(n1, n2)
-    node_for(n1).root = node_for(n2)
+    node_1 = node_for(n1)
+    node_2 = node_for(n2)
+    if node_1.size < node_2.size
+      node_1 << node_2
+    else
+      node_2 << node_1
+    end
   end
 
-  def connected?(n1, n2)
+  def linked?(n1, n2)
     node_for(n1).root == node_for(n2).root
   end
 
@@ -49,8 +63,6 @@ class Union
       "#{node.data} => #{node.root.data}"
     end.inspect
   end
-
-  private
 
   def node_for(data)
     @nodes.find { |n| n.data == data }
--- spec/lib/union_spec.rb
+++ spec/lib/union_spec.rb
@@ -6,11 +6,25 @@ RSpec.describe Union do
 
   it 'connects correctly' do
     subject.link 4, 6
-    expect(subject.connected?(4, 6)).to eq(true)
+    expect(subject.linked?(4, 6)).to eq(true)
     subject.link 3, 6
-    expect(subject.connected?(3, 6)).to eq(true)
+    expect(subject.linked?(3, 6)).to eq(true)
     subject.link 5, 3
-    expect(subject.connected?(5, 6)).to eq(true)
-    expect(subject.connected?(5, 3)).to eq(true)
+    expect(subject.linked?(5, 6)).to eq(true)
+    expect(subject.linked?(5, 3)).to eq(true)
   end
+
+  it 'does not care about order' do
+    subject.link 4, 6
+    subject.link 4, 3
+    expect(subject.linked?(3, 4)).to eq(true)
+  end
+
+  it 'does not overwrite links' do
+    subject.link 1,2
+    subject.link 3,4
+    subject.link 1,5
+    expect(subject.linked?(2,5)).to eq(true)
+  end
+
 end
